e9943fb9f400fcd29ab0cfbca55d81b97dc55804
nm-woodward <nowoodward15@gmail.com>
2022-02-18 17:30:39 -0800

    Finished playRound and game functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-console.log('test')
+// Set up scoring
+let playerScore = 0;
+let computerScore = 0;
 
 
 function randomIntFromInterval(min, max) { // min and max included 
@@ -30,6 +32,56 @@ function playRound() {
     let computerChoice = computerPlay();
     console.log(playerChoice);
     console.log(computerChoice);
+
+    let gameResultMessage;
+
+    if (playerChoice === computerChoice) {
+        gameResultMessage = `You both chose ${playerChoice}. It's a draw!`;
+    }
+
+    else {
+        switch (playerChoice) {
+            case 'rock':
+                gameResultMessage = (computerChoice === 'scissors') ? 'You win!' : 'You lose...';
+                (computerChoice === 'scissors') ? playerScore++ : computerScore ++;
+                break;
+
+            case 'paper':
+                gameResultMessage = (computerChoice === 'rock') ? 'You win!' : 'You lose...';
+                (computerChoice === 'rock') ? playerScore++ : computerScore ++;
+                break;
+
+            case 'scissors':
+                gameResultMessage = (computerChoice === 'paper') ? 'You win!' : 'You lose...';
+                (computerChoice === 'rock') ? playerScore++ : computerScore ++;
+                break;
+        }
+    }
+    console.log(gameResultMessage);
+
 }
 
-playRound();
+function game() {
+    for (let i=0; i<5; i++) {
+        playRound();
+        console.log(`Round ${i+1} completed.`)
+        console.log(`Here\'s the updated score - You: ${playerScore}, Computer: ${computerScore}`);
+    }
+
+    console.log('With all 5 rounds complete, the winner is...');
+    
+    if (playerScore === computerScore) {
+        console.log('Nobody! You both tied');
+    }
+    
+    else if (playerScore > computerScore) {
+        console.log('You!');
+    }
+
+    else {
+        console.log('Not you! Better luck next time.');     
+    }
+
+}
+
+game();
